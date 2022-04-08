96af1724f1902781e4ccf11a15c3c8e6bee56e2a
roman.vorotnyak <Roman.Vorotnjak_ext@leroymerlin.ru>
2022-04-08 18:54:10 +0300

    fix bug

--- src/test/java/cloud/autotests/tests/TrueconfTests.java
+++ src/test/java/cloud/autotests/tests/TrueconfTests.java
@@ -1,17 +1,13 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.helpers.DriverUtils;
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.error.ShouldBe.shouldBe;
 
 
 public class TrueconfTests extends TestBase {
@@ -48,23 +44,14 @@ public class TrueconfTests extends TestBase {
     }
 
     @Test
-    @DisplayName("На странице 'Контакты' указан актуальный номер")
-    void CheckPhoneNumber() {
+    @DisplayName("Кнопка 'Онлайн-консультация' активна")
+    void CheckRegistrationButton() {
         step("Открыть https://trueconf.ru/", () -> {
             open("https://trueconf.ru/");
         });
 
-        step("Клик по 'Компания'", () -> {
-            $(byText("Компания")).click();
-
-        });
-
-        step("Клик по 'Контакты'", () -> {
-            $(".h4").click();
-        });
-
-        step("В заголовке страницы содержится текст 'Тарифы TrueConf Online'", () -> {
-            $("li.contacts").find(String.valueOf(text("8 (800) 101-60-66"))).shouldBe(visible);
+        step("Кнопка 'онлайн-консультация' кликабельна", () -> {
+            $(".chat-button-wrapper__button z-depth-1").shouldBe(enabled);
         });
     }
 
